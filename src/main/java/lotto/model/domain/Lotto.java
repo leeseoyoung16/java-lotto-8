2c83b5046145e00e237be7196f6d81a4574e1457
seo <tjdud11160@gmail.com>
2025-11-03 17:40:38 +0900

    feat: 로또 번호 중복 검증 및 번호 조회 메서드 추가

--- src/main/java/lotto/model/domain/Lotto.java
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -1,6 +1,10 @@
-package lotto;
+package lotto.model.domain;
 
+import lotto.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +18,15 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        Set<Integer> unique = new HashSet<>(numbers);
+        if (unique.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
+        }
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
